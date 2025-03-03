2b1b4c42145fc3d87b2cadf14db0dd89823227e7
Jean-Martin Albert <jm.albert@gmail.com>
2025-03-03 12:30:47 -0500

    Updates to default terminal font

--- wezterm.lua
+++ wezterm.lua
@@ -2,8 +2,10 @@ local wezterm = require 'wezterm'
 local config = {}
 
 config.color_scheme = 'Tokyo Night'
-config.font = wezterm.font( 'JetBrains Mono' )
-config.font_size = 9.0
+
+-- I don't want ligatures!!!
+config.font = wezterm.font( 'JetBrainsMonoNL Nerd Font' )
+config.font_size = 9
 config.default_prog = { "C:\\Program Files\\PowerShell\\7-preview\\pwsh.exe" }
 
 return config
