3fedbf0bf8d4238c5ccf843bc25a861ef4086655
Jean-Martin Albert <jm.albert@gmail.com>
2025-03-03 22:05:55 -0500

    Added zsh as a potential shell to launch

--- wezterm.lua
+++ wezterm.lua
@@ -1,11 +1,45 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
+local os = require("os")
+
 local config = {}
 
-config.color_scheme = 'Tokyo Night'
+--- Check if a file or directory exists in this path
+local function exists(file)
+	local ok, err, code = os.rename(file, file)
+	if not ok then
+		if code == 13 then
+			-- Permission denied, but it exists
+			return true
+		end
+	end
+	return ok, err
+end
+
+config.color_scheme = "Tokyo Night"
 
 -- I don't want ligatures!!!
-config.font = wezterm.font( 'JetBrainsMonoNL Nerd Font' )
+config.font = wezterm.font("JetBrainsMonoNL Nerd Font")
 config.font_size = 9
-config.default_prog = { "C:\\Program Files\\PowerShell\\7-preview\\pwsh.exe" }
+
+local function default_program_powershell()
+	local powershell = nil
+	local program_files = "C:\\Program Files"
+	local powershell_options = { "7_preview", "7" }
+	for _, version in pairs(powershell_options) do
+		local P = program_files .. "\\" .. "PowerShell" .. "\\" .. version .. "\\" .. "pwsh.exe"
+		if exists(P) then
+			powershell = P
+			break
+		end
+	end
+
+	return { powershell, "-nol" }
+end
+
+local function default_program_zsh()
+	return { "C:\\git-sdk-64\\msys2_shell.cmd", "-defterm", "-here", "-no-start", "-ucrt64", "-shell", "zsh" }
+end
+config.default_prog = default_program_zsh()
+config.default_prog = default_program_powershell()
 
 return config
